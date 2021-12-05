454dc96a913c62ef7856251ef535f08f11aadd58
“Anib999” <saliv.mah@gmail.com>
2021-12-05 15:50:56 +0545

    Full change

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,83 +1,72 @@
-import { Modal, Table } from 'antd';
+import { Modal, message } from 'antd';
 import pMinDelay from 'p-min-delay';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import useToken from './useToken';
-// import { useDispatch } from 'react-redux';
-// import { getLoginApi } from '../../services/loginService';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { useDispatch } from 'react-redux';
+import { getLoginApi } from '../../services/loginService';
+import { Form, Input, Button } from 'antd';
 import styled from 'styled-components';
 
 function success() {
   Modal.success({
     content: 'Welcome user',
-    
+
   });
 }
 
 
 export default function Login() {
-  // const { setToken } = props;
-  // const dispatch = useDispatch();
-  const {token, setToken} = useToken();
+  const dispatch = useDispatch();
+  const { token, setToken } = useToken();
   const history = useHistory();
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+  // const [username, setUsername] = useState();
+  // const [password, setPassword] = useState();
 
   const handleSubmit = async e => {
     e.preventDefault();
+  }
+
+
+  const onFinish = (values) => {
     let data = {
-      user: username, 
-      pass: password
+      user: values?.username,
+      pass: values?.password
     }
-    ///here function call
-    if(data.user === 'admin' && data.pass === 'admin'){
+    dispatch(getLoginApi(data, (val) => {
+      // if (val.length !== 0) {
+      if (data.user === 'admin' && data.pass === 'admin') {
+        setToken({ token: data.user, username: 'Anib', roleId: 1, UId: 2 })
+        history.push({
+          pathname: '/'
+        })
+        pMinDelay(success(), 2000);
+      } else {
+        message.error('Username or password incorrect')
+      }
+    }))
+    /* if(data.user === 'admin' && data.pass === 'admin'){
       setToken({token: data.user, username: 'Anib', roleId: 1, UId: 2})
       // setTimeout(() => {
-        // if(token !== undefined){
+      //   if(token !== undefined){
           history.push({
             pathname: '/'
           })
-          pMinDelay(success(), 2000);
-        // }
+          // pMinDelay(success(), 2000);
+      //   }
       // }, 1000);
     }else{
       console.log('noo');
-    }
-    // dispatch(getLoginApi(data, (val) => {
-    //   console.log(val);
-    // }))
-  }
+    } */
+  };
 
-  
-    const onFinish = (values) => {
-      let data = {
-        user: values?.username, 
-        pass: values?.password
-      }
-      ///here function call
-      if(data.user === 'admin' && data.pass === 'admin'){
-        setToken({token: data.user, username: 'Anib', roleId: 1, UId: 2})
-        // setTimeout(() => {
-        //   if(token !== undefined){
-            history.push({
-              pathname: '/'
-            })
-            // pMinDelay(success(), 2000);
-        //   }
-        // }, 1000);
-      }else{
-        console.log('noo');
-      }
-    };
-  
-    const onFinishFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
   return (
     <LoginFormContainer>
-    {/* <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
       <label>
         <p>Username</p>
         <input onChange={val => { setUsername(val.target.value) }} type="text" />
@@ -90,49 +79,49 @@ export default function Login() {
         <button type="submit">Submit</button>
       </div>
     </form> */}
-    <div className="logo">
-      <img src="./Assets/images/logo.png" alt="" />
-    </div>
+      <div className="logo">
+        <img src="./Assets/images/logo.png" alt="" />
+      </div>
 
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      // initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        // initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       // onSubmitCapture={handleSubmit}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-        onChange={val => { setUsername(val.target.value) }}
       >
-        <Input autoFocus="true" />
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        // onChange={val => { setUsername(val.target.value) }}
+        >
+          <Input autoFocus="true" />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-        onChange={val => { setPassword(val.target.value) }} 
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        // onChange={val => { setPassword(val.target.value) }}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
         <Checkbox>Remember me</Checkbox>
       </Form.Item> */}
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-       
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+
+        </Form.Item>
+      </Form>
     </LoginFormContainer>
   )
 }
