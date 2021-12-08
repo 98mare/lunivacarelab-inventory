b90a022782abbce842be68d11333833a100d20d5
“Anib999” <saliv.mah@gmail.com>
2021-12-08 15:46:47 +0545

    asd

--- src/Components/Reports/index.js
+++ src/Components/Reports/index.js
@@ -74,5 +74,10 @@ const ReportContainer = styled.div`
     border-radius: 10px;
     border: 1px solid rgba( 255, 255, 255, 0.18 );
     }
+    @media (max-width: 768px) {
+      a {
+        width: 100%;
+      }
+    }
   }
   `
