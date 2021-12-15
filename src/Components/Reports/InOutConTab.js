d2e2f8120fce87a5043786fd228ceba507bfe3fd
“Anib999” <saliv.mah@gmail.com>
2021-12-15 16:15:24 +0545

    overall changes

--- src/Components/Reports/InOutConTab.js
+++ src/Components/Reports/InOutConTab.js
@@ -16,6 +16,7 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { Table } from 'antd';
+import { getActualConsumApi } from '../../services/stockService';
 
 ChartJS.register(
     LinearScale,
@@ -59,6 +60,7 @@ const InOutConTab = () => {
     const [inData, setInData] = useState([]);
     const [outData, setOutData] = useState([]);
     const [allGoodsList, setAllGoodsList] = useState([]);
+    const [consumptionList, setconsumptionList] = useState([]);
 
     const columns = [
         {
@@ -104,6 +106,36 @@ const InOutConTab = () => {
 
     const labels = label;
 
+    useEffect(() => {
+        getConsuData()
+    }, [label])
+
+    const getConsuData = () => {
+        let data = {
+            fromdate: '2021-10-01',
+            todate: '2021-12-15'
+        }
+
+        dispatch(
+            getActualConsumApi(data, (val) => {
+                let labels = label;
+                let datas = val;
+                if (datas !== null && datas !== undefined) {
+                    const filledMonths = datas.map((month) => month.ItemName);
+                    const dataset = labels.map(month => {
+                        const indexOfFilledData = filledMonths.indexOf(month);
+                        if (indexOfFilledData !== -1) {
+                            return datas[indexOfFilledData].Consumption;
+                        }
+                        return null;
+                    });
+                    setconsumptionList(dataset)
+                }
+
+            })
+        )
+    }
+
     const dataBar = {
         labels,
         datasets: [
@@ -119,18 +151,25 @@ const InOutConTab = () => {
                 data: outData,
                 borderWidth: 2
             },
+            {
+                type: 'line',
+                label: 'Consumption',
+                backgroundColor: '#e95b29',
+                data: consumptionList,
+                borderWidth: 2
+            }
         ],
     };
 
 
     return (
         <>
-            <PageHeader 
+            <PageHeader
                 pageTitle="Total Goods In and Goods Out"
                 csvLinkTitle='Export csv'
                 csvData={allGoodsList}
                 csvDataName='toalReport.csv'
-             />
+            />
             <Bar options={options} data={dataBar} />
             <div className="tableisRes">
                 <Table
